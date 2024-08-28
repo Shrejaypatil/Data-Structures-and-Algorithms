3d6f119b7349a26537151aa02fe41c889ad849d2
Shrejaypatil <119615982+itshre246@users.noreply.github.com>
2024-08-28 19:00:42 +0530

    resolve

--- linked list/Singly Linked List/Singly_Linked_List.cpp
+++ linked list/Singly Linked List/Singly_Linked_List.cpp
@@ -1,50 +1,37 @@
 #include <iostream>
 using namespace std;
 
+// singly ll
+
 class Node
 {
 public:
     int data;
     Node *next;
 
+    // constructor
     Node(int d)
     {
-        this->next = NULL;
         this->data = d;
+        this->next = NULL;
     }
 
     // destructor
-
     ~Node()
     {
-        int popVal = this->data;
-
+        int value = this->data;
         if (this->next != NULL)
         {
-            this->next = NULL;
             delete next;
+            this->next = NULL;
         }
-
-        cout << "The deleted data is: " << popVal << endl;
+        cout << "Node is deleted with data: " << value << endl;
     }
 };
-
-/*
-Operations:
-
-1. insertAtHead
-2. insertAtTail
-3. insertAtPos
-4. deleteAtHead
-5. deleteAtTail // cant delete at tail as we may loose tail link
-6. deleteAtPos
-
-*/
 
 void insertAtHead(Node *&head, int d)
 {
     Node *temp = new Node(d);
-
     temp->next = head;
     head = temp;
 }
@@ -52,174 +39,178 @@ void insertAtHead(Node *&head, int d)
 void insertAtTail(Node *&tail, int d)
 {
     Node *temp = new Node(d);
-
     tail->next = temp;
-
     tail = temp;
 }
 
 void insertAtPos(Node *&head, Node *&tail, int pos, int d)
 {
+    Node *temp = head;
+
     if (pos == 1)
     {
         insertAtHead(head, d);
+        return;
     }
-    else
+
+    int cnt = 1;
+
+    while (cnt < pos - 1)
     {
-        Node *curr = head;
-        Node *prev = head;
-
-        int cnt = 0;
-
-        while (cnt < pos - 1)
-        {
-            prev = curr;
-            curr = curr->next;
-            cnt++;
-        }
-
-        if (curr->next == NULL)
-        {
-            insertAtTail(tail, d);
-        }
-
-        Node *nodeToInsert = new Node(d);
-
-        prev->next = nodeToInsert;
-        nodeToInsert->next = curr;
+        temp = temp->next;
+        cnt++;
     }
+
+    // insert at last
+    if (temp->next == NULL)
+    {
+        insertAtTail(tail, d);
+        return;
+    }
+
+    Node *nodeToInsert = new Node(d);
+    nodeToInsert->next = temp->next;
+    temp->next = nodeToInsert;
 }
 
-void deleteAtHead(Node *&head)
+void printL(Node *&head)
 {
-
-    // first check if single element in list
-    if (head->next == NULL)
+    if (head == NULL)
     {
-        delete head;
+        cout << "List is empty " << endl;
+        return;
     }
-    else
+    Node *temp = head;
+
+    while (temp != NULL)
     {
-        Node *temp = head;
-
-        head = head->next;
-
-        temp->next = NULL;
-
-        delete temp;
+        cout << temp->data << " ";
+        temp = temp->next;
     }
+    cout << endl;
 }
 
-void deleteAtPos(Node *&head, Node *&tail, int pos)
+void deleteAtNode(Node *&head, int pos)
 {
     if (pos == 1)
     {
-        deleteAtHead(head);
+        Node *temp = head;
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
     }
     else
     {
         Node *curr = head;
-        Node *prev = head;
+        Node *prev = NULL;
+        int cnt = 1;
 
-        int cnt = 0;
-
-        while (cnt < pos - 1)
+        while (cnt < pos)
         {
             prev = curr;
             curr = curr->next;
             cnt++;
-        }
-
-        if (curr->next == NULL)
-        {
-            tail = prev;
         }
 
         prev->next = curr->next;
         curr->next = NULL;
+
         delete curr;
     }
 }
 
-void display(Node *head)
+Node *reverseL(Node *head, Node *tail)
 {
-    Node *temp = head;
-    while (temp)
+    if (head == NULL || head->next == NULL)
     {
-        cout << temp->data << "->";
-        temp = temp->next;
+        return head;
     }
-    cout << "NULL";
-    cout << endl;
+
+    Node *curr = head;
+    Node *prev = NULL;
+    Node *forward = NULL;
+
+    while (curr != NULL)
+    {
+        forward = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = forward;
+    }
+
+    return prev;
+}
+
+void detectCycle(Node *head)
+{
+    Node *p1 = head;
+    Node *p2 = head;
+
+    while (p2->next != NULL && p2->next->next != NULL)
+    {
+        p1 = p1->next;
+        p2 = p2->next->next;
+        if (p1 == p2)
+        {
+            cout << "Cycle detected " << endl;
+            return;
+        }
+    }
+    cout << "Cycle not detected" << endl;
+}
+
+void cycle(Node *&head, Node *&tail)
+{
+    tail->next = head->next->next;
 }
 
 int main()
 {
-    Node *root = new Node(10);
+    Node *node1 = new Node(5);
 
-    Node *head = root;
-
-    Node *tail = root;
+    Node *head = node1;
+    Node *tail = node1;
+    printL(head);
 
     insertAtHead(head, 20);
+    printL(head);
 
-    display(head);
+    insertAtTail(tail, 40);
+    printL(head);
 
-    insertAtTail(tail, 30);
+    insertAtPos(head, tail, 4, 1);
+    printL(head);
 
-    display(head);
+    insertAtPos(head, tail, 2, 1);
+    printL(head);
 
-    insertAtPos(head, tail, 2, 100);
+    deleteAtNode(head, 5);
 
-    display(head);
+    printL(head);
 
-    insertAtTail(tail, 60);
+    insertAtTail(tail, 50);
+    printL(head);
 
-    display(head);
+    insertAtTail(tail, 100);
+    printL(head);
 
-    insertAtPos(head, tail, 3, 1101);
+    insertAtTail(tail, 84);
+    printL(head);
 
-    display(head);
+    insertAtTail(tail, -23);
+    printL(head);
 
-    insertAtPos(head, tail, 5, 5001);
+    insertAtTail(tail, 123);
+    printL(head);
 
-    display(head);
+    insertAtTail(tail, 1000);
+    printL(head);
 
-    insertAtHead(head, 70);
+    cycle(head, tail);
 
-    display(head);
+    // mid(head);
 
-    deleteAtHead(head);
-
-    display(head);
-
-    deleteAtPos(head, tail, 3);
-
-    display(head);
-
-    deleteAtPos(head, tail, 6);
-
-    display(head);
-
-    insertAtTail(tail, 450);
-
-    display(head);
-
-    insertAtTail(tail, 80);
-
-    display(head);
-
-    deleteAtPos(head, tail, 7);
-
-    display(head);
-
-    insertAtTail(tail, 80);
-
-    display(head);
-
-    deleteAtPos(head, tail, 4);
-
-    display(head);
+    detectCycle(head);
 
     return 0;
 }
